ccc88087a6badd3ee3e400423b96cbc5cff2ee4b
Jigar Sompura <jigar_sompura@clientjoy.io>
2023-02-07 19:49:49 +0530

    bjugkuf

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -10,60 +10,60 @@ class TaskController extends Controller
 {
     public function addTask(Request $req)
     {
-        
-       
+
+
         // dd($req->all());
         // assign::create($forNewEntry);
         // dd("t");
-        
-        
-    //    $valid_data = 
-    //    [
-    //     'title' => $req->data['title'],
-    //     'description' => $req->data['description'],
-    //     'owner' => $req->data['owner'],
-    //     'created_at'=> $req->data['created_at'],
-    //     'due_date' => $req->data['due_date']
-    //    ]; 
-    
-       $n = new task();
-       $n->title = $req->data['title'];
-       $n->description = $req->data['description'];
-       $n->owner_id = $req->data['owner_id'];
-       $n->created_at = $req->data['created_at'];
-       $n->due_date = $req->data['due_date'];
-       $n->assigned_to = $req->data['assigned_to'];
+
+
+        //    $valid_data =
+        //    [
+        //     'title' => $req->data['title'],
+        //     'description' => $req->data['description'],
+        //     'owner' => $req->data['owner'],
+        //     'created_at'=> $req->data['created_at'],
+        //     'due_date' => $req->data['due_date']
+        //    ];
+
+        $n = new task();
+        $n->title = $req->data['title'];
+        $n->description = $req->data['description'];
+        $n->owner_id = $req->data['owner_id'];
+        $n->created_at = $req->data['created_at'];
+        $n->due_date = $req->data['due_date'];
+        $n->assigned_to = $req->data['assigned_to'];
         $n->save();
 
         // $task = task::create($valid_data);
 
-     
-      
+
+
         // $assign = new assign();
         // $assign->$req->task_id = $task->id;
         // $assign->$req->user_id = $req->assigned_to;
         // $assign->save();
 
-       
-    //    $assign_data =[
-    //     'user_id' => $req->data->assigned_to,
-    //     'task_id' => $task->id
-    //    ];
+
+        //    $assign_data =[
+        //     'user_id' => $req->data->assigned_to,
+        //     'task_id' => $task->id
+        //    ];
         //
 
-    //    $newT = new assign();
-    //    $newT->user_id = $req->data['assigned_to'];
-    //    $newT->task_id = $n->id;
+        //    $newT = new assign();
+        //    $newT->user_id = $req->data['assigned_to'];
+        //    $newT->task_id = $n->id;
 
-    //    $newT->save();
+        //    $newT->save();
 
-       //
+        //
 
-    //    assign::create($assign_data);
+        //    assign::create($assign_data);
 
         // return response()->json(['message' => 'task submitted successfully']);
-        
-        
+
+
     }
 
     public function showTasks()
@@ -71,11 +71,10 @@ class TaskController extends Controller
         // $tasks = task::all();
         // return $tasks;
         $tasks = task::all();
-        
+
         // dd($tasks);
 
-        foreach($tasks as $task)
-        {
+        foreach ($tasks as $task) {
             // dd($task->owner);
             // $task->owner_id;
             // dd($task->owner->name);
@@ -87,9 +86,9 @@ class TaskController extends Controller
             // $taskRow = $task->assignedTo;
             // dd($taskRow);
 
-            $task->assigned_to_name= $task->assignedTo->name;
-            dd($task);
-            
+            $task->assigned_to_name = $task->assignedTo->name;
+            // dd($task);
+
         }
         return $tasks;
         // $tasks = task::first();
@@ -100,11 +99,11 @@ class TaskController extends Controller
         //     'title' => 'asjnak',
         //     'description' => 'akjsdahsj'
         // ];
-        
+
         // $task['c'] = c;
         // $task->c = c;
 
-        
+
 
         $uTask = task::find(1);
         $owner = $uTask->owner;
@@ -113,6 +112,9 @@ class TaskController extends Controller
 
     public function updateTask(Request $req)
     {
+    }
 
+    public function deleteTask(Request $req)
+    {
     }
 }
